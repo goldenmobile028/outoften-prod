8d5cc66c200857ec9ebff6008735e06013938b23
amdurrell <adurrell@gmail.com>
2015-12-18 11:54:37 -0800

    mesage1

--- hello.py
+++ hello.py
@@ -1,7 +1,11 @@
 import os
 from flask import Flask
+from flask.ext.autodoc import Autodoc
+from flask import jsonify
 
 app = Flask(__name__)
+auto = Autodoc(app)
+#decorate each endpoint with @auto.doc() to generate the docs 
 
 @app.route('/')
 def hello():
@@ -13,4 +17,18 @@ def dummy1():
 	
 @app.route('/endpointtwo')
 def dummy2():
-	return 'Endpoint 2' 	    
+	return jsonify(endpoint2="hi")	   
+	
+#Global photo count 
+#@app.route('/v1/photos/count')
+#def get_count():
+#	count = Count.
+#	return jsonify(count=count)	 	 
+
+#Documentation Auto-generator 	
+@app.route('/documentation')
+def documentation():
+    return auto.html()
+    
+#app.debug = True
+#app.run()    	
