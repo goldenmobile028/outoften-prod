0fb5c6ce6314996d061f6e78104fee53ae444196
amdurrell <adurrell@gmail.com>
2015-12-18 13:20:16 -0800

    mesage1

--- hello.py
+++ hello.py
@@ -19,11 +19,47 @@ def dummy1():
 def dummy2():
 	return jsonify(endpoint2="hi")	   
 	
-#Global photo count 
-#@app.route('/v1/photos/count')
+#Provide global photo count 
+@app.route('/v1/photos/count/')
 #def get_count():
 #	count = Count.
-#	return jsonify(count=count)	 	 
+#	return jsonify(count=count)
+
+#Create photo record 
+@app.route('/v1/photos/')
+#def get_count():
+#	count = Count.
+#	return jsonify(count=count)
+
+#Provide list of scores 
+@app.route('/v1/photos/score')
+#def get_count():
+#	count = Count.
+#	return jsonify(count=count)	 
+
+#Delete photos
+@app.route('/v1/photos/<photo_id>')
+#def get_count():
+#	count = Count.
+#	return jsonify(count=count)	 
+
+#Submit a rating 
+@app.route('/v1/photos/rate/')
+#def get_count():
+#	count = Count.
+#	return jsonify(count=count)	 
+
+#Get list of photos to rate 
+@app.route('/v1/photos/rating_list')
+#def get_count():
+#	count = Count.
+#	return jsonify(count=count)	 
+
+#Flagging 
+@app.route('/v1/photos/flag')
+#def get_count():
+#	count = Count.
+#	return jsonify(count=count)	 	 	 	 
 
 #Documentation Auto-generator 	
 @app.route('/documentation')
