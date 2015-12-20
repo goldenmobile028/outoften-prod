7de79b0a35c397e1020e569e2f81455dd94d0c96
amdurrell <adurrell@gmail.com>
2015-12-20 14:01:58 -0800

    model code

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
 import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from flask.ext.autodoc import Autodoc
 from flask import jsonify
 from flask import request 
+#python driver for postgres 
 import psycopg2
 import urlparse
 
 app = Flask(__name__)
 auto = Autodoc(app)
-#decorate each endpoint with @auto.doc() to generate the docs 
+#TODO decorate each endpoint with @auto.doc() to generate the docs 
+
+#Postgres connection stuff  
 
 urlparse.uses_netloc.append("postgres")
 url = urlparse.urlparse(os.environ["DATABASE_URL"])
@@ -21,15 +25,45 @@ conn = psycopg2.connect(
     port=url.port
 )
 
-#Models
+#MODELS
+
+FLAG_STATUS_NONE			= 0
+FLAG_STATUS_AWAITING_REVIEW	= 1
+FLAG_STATUS_APPROVED		= 2
+FLAG_STATUS_BANNED			= 3
+FLAG_STATUS_AUTOBANNED		= 4
+
+CATEGORY_TYPE_LANDSCAPE		= 0
+CATEGORY_TYPE_SELFIE		= 1
+CATEGORY_TYPE_RIDES			= 3
+CATEGORY_TYPE_RANDOM		= 4
+
 
 #User object (device id)
-#class User(db.Model):
-#    uuid = db.Column(db.Integer, primary_key=True)
+class User(db.Model):
+    uuid = db.Column(db.Integer, primary_key=True)
+
+#Table that records a uuid associated with a photo id to avoid showing repeat photos 
+exclusions = db.Table('exclusions',
+	db.Column('uuid', db.Integer),
+	db.Column('photo_id', db.Integer))
+
+#Photo object
+class Photo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    image_url = db.Column(db.Text)
+    category = db.Column(db.Integer)
+	rating_sum = db.Column(db.Integer)
+    rating_total = db.Column(db.Integer)
+    creation_date = db.Column(db.DateTime)
+    flag_count_inappropriate = db.Column(db.Integer)
+    flag_count_miscategorized = db.Column(db.Integer)
+    flag_status = db.Column(db.Integer)
+    	   
 
 #db.create_all()
 
-#Routing
+#ENDPOINTS
 
 @app.route('/')
 def hello():
