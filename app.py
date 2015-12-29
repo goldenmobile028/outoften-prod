fbfe208e659d57beab1c5a037ea658f55ddea710
amdurrell <adurrell@gmail.com>
2015-12-29 13:57:14 -0800

    disabling staging db population

--- app.py
+++ app.py
@@ -186,7 +186,7 @@ def populateDatabase():
 		photo.flag_status = FLAG_STATUS_AWAITING_REVIEW
 		db.session.commit()
 
-populateDatabase()
+#populateDatabase()
 
 #ENDPOINTS
 #TODO: Handle the autobanning
